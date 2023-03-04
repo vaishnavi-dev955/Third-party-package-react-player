578bc8116c537b39d374daf2839e3b72e4368323
vaishnavi-dev955 <vaishnavimandala1998@gmail.com>
2023-03-04 17:43:43 +0000

    commit message

---
+++ src/components/VideoPlayer/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+import ReactPlayer from 'react-player'
+
+const VideoPlayer = () => (
+  <div className="video-container">
+    <h1 className="heading">Video Player</h1>
+    <div className="responsive-container">
+      <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+    </div>
+  </div>
+)
+
+export default VideoPlayer
